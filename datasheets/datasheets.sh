8f6ac8759a92d4d4998fe530a8c5968a084aabd3
daniw <dani.winz@bluewin.ch>
2014-12-30 00:11:26 +0100

    Datasheets DRV830x

---
+++ datasheets/datasheets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### Driver ###
+
+# DRV8301
+wget -nc http://www.ti.com/lit/ds/symlink/drv8301.pdf
+# DRV8302
+wget -nc http://www.ti.com/lit/ds/symlink/drv8302.pdf
+# DRV8303
+wget -nc http://www.ti.com/lit/ds/symlink/drv8303.pdf
